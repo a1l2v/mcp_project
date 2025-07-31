b1de35b46a5bf3336d4776a4075b541d35939871
a1l2v <alvinst2005@gmail.com>
2025-07-31 19:29:42 +0530

    assign tasks

--- server.py
+++ server.py
@@ -8,13 +8,17 @@ mcp = FastMCP(
 
 @mcp.tool()
 def get_weather(city):
-    # TODO implement the weather api call
+    # TODO implement the weather api call @a1l2v
     """get the weather of a particular city"""
     return f"The weather in {city} is windy."
 
+def document_search(query):
+    # TODO implement the document check using rag @a1l2v
+    """get the ans from the document"""
+    return f"It is the answer of the {query} from the document"
 @mcp.tool()
 def web_search(query):
-    # TODO implement the web api call using some web Api
+    # TODO implement the web api call using some web Api @pranave-2
     """get the ans from the web"""
     return f"It is the answer of the {query} from the web"
 
